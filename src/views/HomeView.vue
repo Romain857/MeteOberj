16596efc478bfa39ffaa603c5ad96819f781ad65
YohannDurel1 <y.durel@sylvamap.fr>
2022-10-13 14:45:27 +0200

    using fetch for api calls

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <div v-for="post in posts" v-bind:key="post.id">
+      <b>{{ post.title }}</b>
+      <p>{{ post.body }}</p>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: [],
+    };
+  },
+
+  methods: {
+    async getData() {
+      try {
+        let response = await fetch("https://jsonplaceholder.typicode.com/posts");
+        this.posts = await response.json();;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+
+  created() {
+    this.getData();
+  },
+};
+</script>
